d3f27f1b3ff2c65943397de15939468c889f7e7d
lukehart54 <63265093+lukehart54@users.noreply.github.com>
2022-07-20 22:39:20 -0400

    all down hill from here

---
+++ src/components/Options.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    x: number
+    y: number
+}
+
+// Props - Size x, y 
+
+const Options = (props: Props) => {
+    return (
+        <div className="options">
+
+        </div>
+    )
+    }
